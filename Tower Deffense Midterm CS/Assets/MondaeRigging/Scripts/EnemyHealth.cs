363411b3c262081184202c238c4cae799a5fd926
NaijaAvenger <mondaeatughonu@gmail.com>
2022-10-01 22:32:29 -0400

    10/1/22 - xp spawn update

--- Tower Deffense Midterm CS/Assets/MondaeRigging/Scripts/EnemyHealth.cs
+++ Tower Deffense Midterm CS/Assets/MondaeRigging/Scripts/EnemyHealth.cs
@@ -26,7 +26,11 @@ public class EnemyHealth : MonoBehaviour
 
         if (Health <= 0)
         {
-            PhotonNetwork.Instantiate("Loot1b", transform.position, Quaternion.identity);
+            var randNum = Random.Range(0, 4); // this will return a number between 0 and 9 
+            for (var i = 0; i < randNum; i++)
+            {
+                PhotonNetwork.Instantiate("Loot1b", transform.position, Quaternion.identity);
+            }
             Invoke(nameof(DestroyEnemy), 1f);
         }
     }
